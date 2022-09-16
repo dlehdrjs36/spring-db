29646a53ef26398c45cb7498230eeaea6bebdb33
dglee <slsldkf123@naver.com>
2022-09-17 02:32:56 +0900

    connection pool, dataSource 공부

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,8 +1,11 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,8 +17,39 @@ public class ConnectionTest {
 
     @Test
     void driverManager() throws SQLException {
+        //커넥션 생성 시 마다 설정 파라미터 정보 필요. 설정과 사용이 분리안됨
         Connection conn1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection conn2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", conn1, conn1.getClass());
+        log.info("connection={}, class={}", conn2, conn2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        //초기에는 커넥션을 생성하지 않고 설정 정보를 설정하기 위해 파라미터 필요. 이후에 설정된 정보대로 커넥션 생성. 설정과 사용이 분리되어 있음.
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("JdbcConnectionPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection conn1 = dataSource.getConnection();
+        Connection conn2 = dataSource.getConnection();
         log.info("connection={}, class={}", conn1, conn1.getClass());
         log.info("connection={}, class={}", conn2, conn2.getClass());
     }
---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,62 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        //기본 DriverManager - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+
+        //커넥션 풀 - 커넥션 풀에 존재하는 커넥션을 획득.
+//        HikariDataSource dataSource1 = new HikariDataSource();
+//        dataSource1.setJdbcUrl(URL);
+//        dataSource1.setUsername(USERNAME);
+//        dataSource1.setPassword(PASSWORD);
+//        dataSource1.setMaximumPoolSize(10);
+//        repository = new MemberRepositoryV1(dataSource1);
+    }
+
+    @Test
+    void crud() throws SQLException, InterruptedException {
+        //save
+        Member member = new Member("memberV100", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(1000);
+    }
+}
